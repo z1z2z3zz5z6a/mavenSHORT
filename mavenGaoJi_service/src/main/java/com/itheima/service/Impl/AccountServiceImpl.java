d383522ea4b654f3cd3ea809bf6551f8e7ddbb96
root <root@qq.com>
2018-12-16 21:05:42 +0800

    service中test方法

--- mavenGaoJi_service/src/main/java/com/itheima/service/Impl/AccountServiceImpl.java
+++ mavenGaoJi_service/src/main/java/com/itheima/service/Impl/AccountServiceImpl.java
@@ -13,28 +13,27 @@ public class AccountServiceImpl implements AccountService {
     @Autowired
     private AccountDao accountDao;
 
-    @Override
     public List<Items> findAll() {
         return accountDao.findAll();
     }
 
-    @Override
     public void insertOne(Items items) {
         accountDao.insertOne(items);
     }
 
-    @Override
     public void deleteOneById(int id) {
         accountDao.deleteOneById(id);
     }
 
-    @Override
     public void updateOne(Items items) {
         accountDao.updateOne(items);
     }
 
-    @Override
     public Items jumpUpdate(int id) {
         return accountDao.jumpUpdate(id);
+    }
+
+    public void test1(){
+        System.out.println("haha");
     }
 }
